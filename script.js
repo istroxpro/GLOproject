1b19c112ca21e322d4a2d6250df5834fcee6c598
istroxpro <istrodantir@icloud.com>
2021-11-26 16:42:03 +0700

    first homework fixed

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const title = 1;
-const screens = 2;
-const screenPrice = 3;
-const rollback = 4;
-const fullPrice = 5;
-const adaptive = 6;
+let title;
+let screens;
+let screenPrice;
+let rollback;
+let fullPrice;
+let adaptive;
 
 console.log('All things fine');
 
