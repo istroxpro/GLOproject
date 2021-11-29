6ce4ce28893fc6bf18b96db1732c43d074cd92c4
istroxpro <istrodantir@icloud.com>
2021-11-29 18:53:17 +0700

    second homework

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = 'Название проекта';
+let screens = 'Простые, Сложные, Интерактивные'
+let screenPrice = 17;
+let rollback = 50;
+let fullPrice = 500;
+let adaptive = true;
 
-console.log('All things fine');
-
-alert('wegwge');
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log(`Стоимость верстки экранов ${screenPrice} рублей`)
+console.log(`Стоимость разработки сайта ${fullPrice} рублей`)
+screens.toLocaleLowerCase();
+screens.split(', ');
+console.log(screens);
+console.log(fullPrice * (rollback / 100))
