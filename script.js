aaecba3bcae6e3cd8db2bbb4edacc8687b60fe9b
istroxpro <istrodantir@icloud.com>
2021-12-01 15:44:40 +0700

    third homework

--- script.js
+++ script.js
@@ -15,3 +15,28 @@ screens.toLocaleLowerCase();
 screens.split(', ');
 console.log(screens);
 console.log(fullPrice * (rollback / 100))
+// Третий урок
+title = prompt('Как называется ваш проект?');
+screens = prompt('Какие типы экранов нужно разобрать?');
+screenPrice = +prompt('Сколько будет стоить данная работа?');
+adaptive = confirm('Нужен ли адаптив на сайте?')
+// Создаю переменные с вопросами о дополнительных услугах
+let service1 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice1 = +prompt('Сколько это будет стоить?');
+let service2 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice2 = +prompt('Сколько это будет стоить?');
+// Вычисление итоговой стоимости работ
+fullPrice = screenPrice + servicePrice1 + servicePrice2;
+// Итоговая стоимость за вычетом отката
+let servicePercentPrice = fullPrice - Math.ceil(rollback);
+console.log(servicePercentPrice);
+// Конструкция условий
+if (fullPrice > 30000) {
+    console.log('Даем скидку в 10%')
+} else if (fullPrice > 15000 && fullPrice < 30000) {
+    console.log('Даем скидку в 5%')
+} else if (fullPrice < 15000 && fullPrice > 0) {
+    console.log('Скидка не предусмотрена')
+} else {
+    console.log('Что-то пошло не так')
+};
