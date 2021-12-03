df7d72eb163e144fe83118a63a5cf4d9a7734d65
istroxpro <istrodantir@icloud.com>
2021-12-03 14:28:36 +0700

    fifth homework

--- script.js
+++ script.js
@@ -1,30 +1,40 @@
 'use strict';
-
-let title = 'Название проекта';
-let screens = 'Простые, Сложные, Интерактивные'
-let screenPrice = 17;
+// Блок для объявления функций.
+let title;
+let screens;
+let screenPrice;
 let rollback = 50;
-let fullPrice = 500;
-let adaptive = true;
+let fullPrice;
+let adaptive;
 let allServicePrices;
+let service1;
+let service2;
+let servicePercentPrice;
 
-title = prompt('Как называется ваш проект?');
-screens = prompt('Какие типы экранов нужно разобрать?');
-screenPrice = +prompt('Сколько будет стоить данная работа?');
-adaptive = confirm('Нужен ли адаптив на сайте?')
+// Проверка на введение в модальное окно для ответа числового значения.
+const isNumber = function (num) {
+    return !isNaN(parseFloat(num)) && isFinite(num)
+};
 
-let service1 = prompt('Какой дополнительный тип услуги нужен?');
-let servicePrice1 = +prompt('Сколько это будет стоить?');
-let service2 = prompt('Какой дополнительный тип услуги нужен?');
-let servicePrice2 = +prompt('Сколько это будет стоить?');
+// Функция, которая позволяет нам своим вызовом задать необходимые вопросы и записать ответы в переменные.
+const asking = function () {
+    title = prompt('Как называется ваш проект?', 'Калькулятор верстки');
+    screens = prompt('Какие типы экранов нужно разобрать?', 'Простые, сложные');
 
-let servicePercentPrice = fullPrice - Math.ceil(rollback);
+    // цикл, который задействует ранее написанную функцию проверки числового значения и задает вопрос.
+    do {
+        screenPrice = prompt('Сколько будет стоить данная работа?')
+    } while (!isNumber(screenPrice))
 
+    adaptive = confirm('Нужен ли адаптив на сайте?')
+}
 
+// Функция позволяет узнать тип данных каждой переменной, которую мы укажем ниже.
 const showTypeOf = function (variable) {
     console.log(variable, typeof variable);
 };
 
+// Функция позволяет узнать размер скидки, которую мы предложим в зависимости от входящих параметров.
 const getRollbackMessage = function (price) {
     if (price >= 30000) {
         return 'Даем скидку в 10%'
@@ -37,36 +47,55 @@ const getRollbackMessage = function (price) {
     };
 };
 
-const getAllServicePrices = function (a, b) {
-    return a + b
+// Функция с циклом, позволяет задать вопросы ограниченное количество раз и записать ответы в переменные.
+const getAllServicePrices = function () {
+    let sum = 0;
+    for (let i = 0; i < 2; i++) {
+        if (i === 0) {
+            service1 = prompt('Какой дополнительный тип услуги нужен?')
+        } else if (i === 1) {
+            service2 = prompt('Какой дополнительный тип услуги нужен?')
+        };
+        sum += (() => {
+            let n;
+            do {
+                n = prompt('Сколько это будет стоить?');
+            } while (!isNumber(n));
+            return +n;
+        })();
+    }
+    return sum;
 };
 
-
-function getFullPrice(a, b) {
-    return a + b
+// Получаем конечную стоимость.
+function getFullPrice() {
+    return +screenPrice + +allServicePrices
 };
 
-
+// Заставили название проекта писаться с заглавной буквы, а весь остальной регистр переводится в нижний.
 function getTitle(str) {
     if (!str) return str;
     return str = str[0].toUpperCase() + str.slice(1).toLowerCase()
 };
 
-
-const getServicePercentPrices = function (a, b) {
-    return a - Math.ceil(b)
+// Показываем сумму отката.
+const getServicePercentPrices = function () {
+    return +fullPrice - rollback
 }
 
-servicePercentPrice = getServicePercentPrices(fullPrice, rollback);
-allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
+// Вызываем функции.
+asking();
+servicePercentPrice = getServicePercentPrices();
+allServicePrices = getAllServicePrices();
 title = getTitle(title);
-fullPrice = getFullPrice(screenPrice, allServicePrices);
+fullPrice = getFullPrice();
 showTypeOf(title);
 showTypeOf(fullPrice);
 showTypeOf(adaptive);
-screens.toLocaleLowerCase();
+screens.toLowerCase();
 screens.split(', ');
 
+// Логи
 console.log(getRollbackMessage(fullPrice));
 console.log(`Стоимость верстки экранов ${screenPrice} рублей`)
 console.log(`Стоимость разработки сайта ${fullPrice} рублей`)
